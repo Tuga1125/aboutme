8737c4be43fb66c7b75d8f7776356d13199a0746
Tooga Garanja Magar <tugamagar77@gmail.com>
2023-09-15 11:53:40 -0400

    Initialize BasicApp

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function AboutMe(){
+    return (
+        <View style ={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+        <Text>Tooga Garanja Magar</Text>
+        <Text>301369368</Text>
+        </View>
+    );
+}
